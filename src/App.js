d474a89616883c97fe6f121029015ab9d28f9cd6
ines-dhawadi <inesdhawadisimplonend@gmail.com>
2021-04-30 12:27:00 +0100

    version final

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 <Convert />
 
 
-<Dynmc />
+
 
     </div>
   );
--- src/components/convert.js
+++ src/components/convert.js
@@ -7,43 +7,77 @@ import {InputGroup,FormControl,Button  } from "react-bootstrap";
 
 class Convert extends React.Component{
 
-    constructor(){
-        super()
-   
-    this.state = {
-        numb : '',
-       
+    state = {
+        numb : 0,
+        h:0,
+        m:0,
+        s:0,
+
 
     }
-
-    /***************************** 
-setInterval(
-    () =>{
-        this.setState({
-            Convert: this.state.numb +1
-        })
-    },
-    1000
-)----------*/
+    start = () => {
+        this.interval = setInterval(() => {
+            let s = this.state.s;
+            if (s === 60){
+                this.state.m++ 
+                this.state.s = 0 
     
+            }
+            if (this.state.m === 60){
+                this.state.h ++
+                this.state.m = 0 
+            }
+    
+             this.setState({
+                 s : Number(this.state.s)+1,
+             })
+         },1000);
+     }
+
+ehange=(e)=>{
+    this.setState({
+        numb:(e.target.value )   })
+
 }
 
-handlechange(event){
-    let val = event.target.value
-    console.log(val)
-    this.setState({numb:val})
-
+conv=()=>{
+  let  time=this.state.numb
+    this.setState({
+        h: Math.floor(time/3600),
+        m:Math.floor((time%3600)/60),
+        s:time % 60
+    })
+    console.log(time)
 }
 
+Reset=(e)=>{
+    
+    this.setState({
+        h: 0,
+        m:0,
+        s:0
+       
+    })
+   
+}
+
+Stop=()=>{
+
+  clearInterval(this.interval)
+  this.setState({
+      interval: ' '
+  })
+}
     render(){ 
         return(
             <div className="globl">
                 {/**************************************** */}
                 <div className="container col-md-6  col-sm-12">
         
-                <h4>Vous pouvez convertir les seconds en ( heures, Minutes, seconds)</h4>
+                <h4 className="mb-4">You can convert seconds to (hours, minutes, seconds)</h4>
                 
-        <InputGroup  className="mb-3 "  onChange={this.handlechange.bind(this)} value={this.state.numb}   required>
+        <InputGroup  className="mb-3 "  onChange={this.ehange}   required>  
+        <Button variant="dark" onClick={this.conv} >Convertir</Button>
             <InputGroup.Prepend>
               <InputGroup.Text id="basic-addon1" ></InputGroup.Text>
             </InputGroup.Prepend>
@@ -53,16 +87,19 @@ handlechange(event){
                 </div>
          {/**************************************** */}
          <div className=" toititl">
-             <h6 className="bnttt">heures : {Math.floor (this.state.numb/3600)} </h6>
-             <h6 className="bnttt">Minutes :{Math.floor (this.state.numb/60)} </h6>
-             <h6 className="bnttt">seconds : {this.state.numb}</h6>
+             <h6 className="bnttt">hours : {String(this.state.h).padStart(2,'0')}</h6>
+             <h6 className="bnttt">minutes :{String(this.state.m).padStart(2,'0')}</h6>
+             <h6 className="bnttt">seconds : {String(this.state.s).padStart(2,'0')}</h6>
 
 
             
          </div>
-         <Button variant="dark">Supprimer</Button>
+<div className="trobnt"> 
+         <Button variant="dark" onClick={this.start}>Start</Button>
+         <Button variant="dark"  onClick={this.Reset}>Reset</Button>
+         <Button variant="dark" onClick={this.Stop}>Stop</Button>
         
-        
+         </div>
             </div>
            )
     }
